69353db51f12565440f2c18f94c97a28bc971e13
VladimirVasilev <vassilev.vlad@gmail.com>
2016-07-06 13:47:16 +0300

    Added locks to the service to handle concurrent calls. The locking scheme is not perfect so there is more work

--- SortingService.BusinessLayer/SessionManager.cs
+++ SortingService.BusinessLayer/SessionManager.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ServiceModel;
 using SortingService.BusinessLayer.SortingAlgorithms;
 using SortingService.DataAccess;
@@ -7,12 +8,21 @@ namespace SortingService.BusinessLayer
 {
     public class SessionManager
     {
-        private DataAccessLayer _dataAccess = new DataAccessLayer();
-        private ImprovedSorting _sortingAlgorithm = new ImprovedSorting();
+        private static readonly object CreationLock = new object();
+        private static readonly Dictionary<Guid, object> SessionLocks = new Dictionary<Guid, object>();
+
+        private readonly DataAccessLayer _dataAccess = new DataAccessLayer();
+        private readonly ImprovedSorting _sortingAlgorithm = new ImprovedSorting();
 
         public Guid StartNewSession()
         {
-            return _dataAccess.CreateNewSession();
+            lock (CreationLock)
+            {
+                var result = _dataAccess.CreateNewSession();
+                SessionLocks[result] = new object();
+
+                return result;
+            }
         }
 
         public void PutStreamData(Guid streamGuid, string[] text)
@@ -22,13 +32,16 @@ namespace SortingService.BusinessLayer
                 throw new FaultException($"No session for the guid provided: {streamGuid}");
             }
 
-            string currentDataFile = _dataAccess.GetSortedSessionFile(streamGuid);
+            // Pesimistic concurrency is used for simplicity. Another option would be to use queues
+            lock (SessionLocks[streamGuid])
+            {
+                // TODO The session may already be deleted
+                string currentDataFile = _dataAccess.GetSortedSessionFile(streamGuid);
+                var mergedFile = _sortingAlgorithm.Sort(text, currentDataFile);
 
-            var mergedFile = _sortingAlgorithm.Sort(text, currentDataFile);
-
-            _dataAccess.SetSortedSessionFile(streamGuid, mergedFile);
+                _dataAccess.SetSortedSessionFile(streamGuid, mergedFile);
+            }
         }
-
 
         public string[] GetStreamData(Guid streamGuid)
         {
@@ -37,7 +50,11 @@ namespace SortingService.BusinessLayer
                 throw new FaultException($"No session for the guid provided: {streamGuid}");
             }
 
-            return _dataAccess.GetDataForSession(streamGuid);
+            lock (SessionLocks[streamGuid])
+            {
+                // TODO Getting the stream content is blocked by a Put operation. For responsiveness maybe have separate locks for reading and writing
+                return _dataAccess.GetDataForSession(streamGuid);
+            }
         }
 
         public void EndStream(Guid streamGuid)
@@ -47,7 +64,10 @@ namespace SortingService.BusinessLayer
                 throw new FaultException($"No session for the guid provided: {streamGuid}");
             }
 
-            _dataAccess.DeleteSession(streamGuid);
+            lock (SessionLocks[streamGuid])
+            {
+                _dataAccess.DeleteSession(streamGuid);
+            }
         }
     }
 }
