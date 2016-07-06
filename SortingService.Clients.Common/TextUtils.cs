4322974d91a4a0dc3c8a9ce440c588cf461fe5e2
MatheMagic <vassilev.vlad@gmail.com>
2016-07-06 22:32:40 +0300

    Rearranged the client project a bit. Added a common project for all clients

--- SortingService.Clients.Common/TextUtils.cs
+++ SortingService.Clients.Common/TextUtils.cs
@@ -3,21 +3,23 @@ using System.Linq;
 
 namespace SortingService.Client
 {
-    internal static class TextUtils
+    public static class TextUtils
     {
         private static Random random = new Random();
 
-        internal static string[] GetRandomAlphanumericData()
+        public static string[] GetRandomAlphanumericData()
         {
             return Enumerable.Range(0, random.Next(20, 100))
-                      .Select(_ => TextUtils.RandomAlphanumericString(random.Next(30, 50)))
-                      .ToArray();
+                .Select(_ => RandomAlphanumericString(random.Next(30, 50)))
+                .ToArray();
         }
 
         private static string RandomAlphanumericString(int length)
         {
             const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz 0123456789";
-            return new string(Enumerable.Range(0, length).Select(x => chars[random.Next(0, chars.Length - 1)]).ToArray());
+            return new string(Enumerable.Range(0, length)
+                .Select(x => chars[random.Next(0, chars.Length - 1)])
+                .ToArray());
         }
     }
 }
