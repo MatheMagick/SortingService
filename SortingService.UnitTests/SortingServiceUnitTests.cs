8bd3c2b1b8d9567135584d73a5f481f934dda9d3
MatheMagic <vassilev.vlad@gmail.com>
2016-07-08 19:54:04 +0300

    Added test projects

---
+++ SortingService.UnitTests/SortingServiceUnitTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SortingService.UnitTests
+{
+    [TestClass]
+    public class SortingServiceUnitTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
