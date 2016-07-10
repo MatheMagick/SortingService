c71732bf6a8100dbccb4ad0b6264b66c88a33a76
VladimirVasilev <vassilev.vlad@gmail.com>
2016-07-10 16:20:11 +0300

    Removed the unused test project, since there were no meaningful tests

---
+++ SortingService.UnitTests/SesssionManagerUnitTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SortingService.BusinessLayer;
+using SortingService.BusinessLayer.SortingAlgorithms;
+using SortingService.DataAccess;
+
+namespace SortingService.UnitTests
+{
+    [TestClass]
+    public class SesssionManagerUnitTests
+    {
+        [TestMethod]
+        public void SessionManger_StartNewSession()
+        {
+            var mockDataAccess = new Mock<IDataAccessLayer>();
+            var mockSorting = new Mock<IImprovedSorting>();
+            ISessionManager manager = new SessionManager(mockDataAccess.Object, mockSorting.Object);
+
+            manager.StartNewSession();
+
+            mockDataAccess.Verify(x => x.CreateNewSession());
+        }
+    }
+}
