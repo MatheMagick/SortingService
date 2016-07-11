8b0728937931eb63ef0a55365dd1eda8d10f21e3
VladimirVasilev <vassilev.vlad@gmail.com>
2016-07-11 08:49:50 +0300

    Changed a setting of 16 GB file client

--- SortingService.Client16GBFile/Settings.Designer.cs
+++ SortingService.Client16GBFile/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace SortingService.Client16GBFile {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("1")]
+        [global::System.Configuration.DefaultSettingValueAttribute("16")]
         public byte FileSizeInGB {
             get {
                 return ((byte)(this["FileSizeInGB"]));
